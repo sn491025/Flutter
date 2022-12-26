e692981120cb8adbbaef8ca6208ef0ce6ad5d796
pro491025 <102411845+pro491025@users.noreply.github.com>
2022-12-26 15:33:29 +0530

    here i push the contact app

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,242 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const Contact());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-  @override
-  // ignore: library_private_types_in_public_api
-  _MyAppState createState() => _MyAppState();
-}
-
-// This is English text For change Text
-String englishText = 'Hi This is Scaffold Page';
-
-// This is Tamil Text For change
-String tamilText = 'ஹாய் இது சாரக்கட்டு பக்கம்';
-
-class _MyAppState extends State<MyApp> {
-  String displayText = englishText;
-
+class Contact extends StatelessWidget {
+  const Contact({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('SANKARA HOME'),
-            leading: const Icon(Icons.home),
-            backgroundColor: const Color.fromARGB(255, 255, 51, 0),
+            leading: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            backgroundColor: Colors.purple,
             actions: <Widget>[
               IconButton(
-                  onPressed: () {
-                    setState(() {
-                      displayText =
-                          displayText == englishText ? tamilText : englishText;
-                    });
-                  },
-                  icon: const Icon(Icons.refresh))
+                onPressed: () {
+                  print('Hi this is Star');
+                },
+                icon: const Icon(
+                  Icons.star_border,
+                  color: Colors.black,
+                  size: 30,
+                ),
+              )
             ],
           ),
-          body: Center(
-            child: Text(
-              displayText,
-              style: const TextStyle(
-                fontSize: 30,
-                color: Color.fromARGB(255, 255, 179, 0),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          body: ListView(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  // Image
+                  Container(
+                    width: double.infinity,
+                    height: 250,
+                    color: Colors.blue,
+                    child: Image.network(
+                      'https://i.pinimg.com/564x/b4/c1/1a/b4c11a320b8edcc8292b75ceea6e871c.jpg',
+                      height: 750,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                  // Name
+                  Container(
+                      height: 70,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Captain America',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 35),
+                            ),
+                          ),
+                        ],
+                      )),
+                  const Divider(
+                    color: Colors.blueGrey,
+                  ),
+
+                  Container(
+                    margin: const EdgeInsets.only(top: 8, bottom: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        buildCallButton(),
+                        buildTextButton(),
+                        buildVideoButton(),
+                        buildEmailButton(),
+                        buildDirectionsButton(),
+                        buildPayButton()
+                      ],
+                    ),
+                  ),
+                  const Divider(
+                    color: Colors.blueGrey,
+                  ),
+
+                  // Contact list
+                  mobilePhoneListTile(),
+                  anotherMobilePhoneListTile(),
+
+                  const Divider(
+                    color: Colors.blueGrey,
+                  ),
+
+                  // Email ListTail
+                  emailListTile(),
+
+                  const Divider(
+                    color: Colors.blueGrey,
+                  ),
+
+                  // Address ListTail
+                  addressListTile(),
+
+                  const Divider(
+                    color: Colors.indigo,
+                  )
+                ],
+              )
+            ],
           )),
-      debugShowCheckedModeBanner: false,
     );
   }
+}
+
+// Call IconButton
+Widget buildCallButton() {
+  return Column(
+    children: <Widget>[
+      IconButton(
+        icon: Icon(
+          Icons.call,
+          color: Colors.indigo.shade800,
+        ),
+        onPressed: () {},
+      ),
+      const Text("Call")
+    ],
+  );
+}
+
+// Text IconButton
+Widget buildTextButton() {
+  return Column(
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.message, color: Colors.indigo.shade800),
+        onPressed: () {},
+      ),
+      const Text("Text")
+    ],
+  );
+}
+
+// Video IconButton
+Widget buildVideoButton() {
+  return Column(
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.video_call, color: Colors.indigo.shade800),
+        onPressed: () {},
+      ),
+      const Text("Video")
+    ],
+  );
+}
+
+// Email IconButton
+Widget buildEmailButton() {
+  return Column(
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.email, color: Colors.indigo.shade800),
+        onPressed: () {},
+      ),
+      const Text("Email")
+    ],
+  );
+}
+
+// Directions IconButton
+Widget buildDirectionsButton() {
+  return Column(
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.directions, color: Colors.indigo.shade900),
+        onPressed: () {},
+      ),
+      const Text("Direction")
+    ],
+  );
+}
+
+// Pay IconButton
+Widget buildPayButton() {
+  return Column(
+    children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.payment, color: Colors.indigo.shade800),
+        onPressed: () {},
+      ),
+      const Text("Pay")
+    ],
+  );
+}
+
+// Mobile Phone ListTile
+Widget mobilePhoneListTile() {
+  return ListTile(
+    leading: Icon(Icons.call, color: Colors.indigo.shade800),
+    title: const Text('7358495594'),
+    subtitle: const Text('Mobile'),
+    trailing: IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.message),
+      color: Colors.indigo.shade800,
+    ),
+  );
+}
+
+// Another Mobile Phone listTile
+Widget anotherMobilePhoneListTile() {
+  return ListTile(
+    leading: Icon(Icons.call, color: Colors.indigo.shade800),
+    title: const Text('7358495594'),
+    subtitle: const Text('Mobile'),
+    trailing: IconButton(
+      onPressed: () {},
+      icon: const Icon(Icons.message),
+      color: Colors.indigo.shade800,
+    ),
+  );
+}
+
+// Email listTile
+Widget emailListTile() {
+  return ListTile(
+    leading: Icon(Icons.email, color: Colors.indigo.shade800),
+    title: const Text('sn491025@gmail.com'),
+    subtitle: const Text('Personal'),
+  );
+}
+
+// Address listTile
+Widget addressListTile() {
+  return ListTile(
+    leading: Icon(Icons.location_on, color: Colors.indigo.shade800),
+    title: const Text('234 IndiraNagar St, London'),
+    subtitle: const Text('Home'),
+    trailing: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.directions, color: Colors.indigo.shade800)),
+  );
 }
